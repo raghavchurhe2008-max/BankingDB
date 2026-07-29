cad890a5f6017b483f994c513a17741c774a5801
raghavchurhe <raghavchurhe2008@gmail.com>
2026-07-29 09:17:35 +0530

    Created New Tables

--- banking.sql
+++ banking.sql
@@ -9,5 +9,39 @@ AccountCreationDate DATE
 );
 
 describe customers;
-
 select * from customers;
+
+CREATE TABLE Accounts (
+AccountID INT,
+AccountType VARCHAR(20),
+Balance DECIMAL(10,2)
+);
+
+desc Accounts;
+
+create table Transactions (
+TransactionID int,
+TransactionDate date,
+Amount decimal(10,2),
+TransactionType varchar(20)
+);
+desc Transactions;
+
+create table Branches (
+BranchID int,
+BranchName varchar(100),
+BranchAddress varchar(200),
+BranchPhone varchar(15)
+);
+
+create table AccountBranches (
+AssignmentDate date
+);
+
+create table Loans (
+LoanID int,
+LoanAmount decimal(10,2),
+InterestRate decimal(5,2),
+StartDate date,
+EndDate date
+);
