a67ba89d90d582842590811e6ebe2d2e2cb27e4f
raghavchurhe <raghavchurhe2008@gmail.com>
2026-07-31 09:12:53 +0530

    Learnt ERD

--- banking.sql
+++ banking.sql
@@ -95,3 +95,40 @@ add constraint
 foreign key (BranchID)
 references Branches(BranchID);
 desc Accounts;
+ 
+ desc transactions;
+ alter table transactions
+ add constraint PK_Transactions
+ primary key (TransactionID);
+ 
+alter table transactions
+add AccountID int;
+
+alter table transactions
+add constraint 
+foreign key (AccountID)
+references accounts(AccountID);
+desc transactions;
+
+alter table loans
+add constraint PK_Loans
+primary key (LoanID);
+
+alter table loans
+add CustomerID int;
+
+alter table loans
+add constraint
+foreign key (CustomerID)
+references customers(CustomerID);
+desc loans;
+
+select
+	constraint_name,
+    constraint_type
+from INFORMATION_SCHEMA.TABLE_CONSTRAINTS
+WHERE TABLE_SCHEMA = 'bankingDB'
+  And TABLE_NAME = 'Acconts';
+  
+  
+  
